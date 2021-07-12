16f8cb3c50af8171962853b480e4d8a67b26d31e
Rayhan Mia <miarayhan11@icloud.com>
2021-07-12 11:04:51 +0600

    lab5

--- Lab5/editProduct.php
+++ Lab5/editProduct.php
@@ -19,7 +19,12 @@ include "nav.php";
   <label for="b_price">Buying Price:</label><br>
   <input value="<?php echo $product['Buying_Price'] ?>" type="text" id="b_price" name="b_price"><br>
   <label for="s_price">Selling Price:</label><br>
-  <input value="<?php echo $product['Selling_Price'] ?>" type="text" id="s_price" name="s_price"><br>
+  <input value="<?php echo $product['Selling_Price'] ?>" type="text" id="s_price" name="s_price"><br> <br>
+
+
+     <input type="checkbox" id="chk" name="display" <?php if ($product['display']){echo 'checked';} ?> >
+     <label for="chk"> Display </label>
+     <br><br>
 
   <input type="hidden" name="id" value="<?php echo $_GET['id'] ?>">
 
