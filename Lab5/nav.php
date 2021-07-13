19a395cab8d5655c914e3f7914c91b1a364c05bb
Rayhan Mia <miarayhan11@icloud.com>
2021-07-13 23:48:56 +0600

    lab5

--- Lab5/nav.php
+++ Lab5/nav.php
@@ -1,5 +1,5 @@
  <ul>
-        <li><a href="addProduct.php"> Add Product</a></li>
-        <li><a href="showAllProducts.php"> Show all Product</a></li>
-        <li><a href="searchProduct.php"> Search Product</a></li>
+        <li><a href="/webtech/Lab5/addProduct.php"> Add Product</a></li>
+        <li><a href="/webtech/Lab5/showAllProducts.php"> Show all Product</a></li>
+        <li><a href="/webtech/Lab5/searchProduct.php"> Search Product</a></li>
 </ul>
