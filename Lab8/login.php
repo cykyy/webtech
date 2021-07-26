b972bba241f592279d55d4181aabb1bf3cafadee
Rayhan Mia <miarayhan11@icloud.com>
2021-07-26 14:10:14 +0600

    lab

---
+++ Lab8/login.php
@@ -0,0 +1,179 @@
+<?php
+session_start();
+include 'templates/nav2.php';?>
+<?php include 'templates/base2.php';?>
+<?php
+// define variables and set to empty values
+$userErr = $passErr = "";
+$username = $password = "";
+$errCount = 0;
+
+if (isset($_SESSION['uname'])) {
+    header('Location: dashboard.php');
+
+} else{
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+        if (empty($_POST["username"])) {
+            $userErr = "Username is required";
+            $errCount = $errCount + 1;
+        } else {
+            $username = check_input($_POST["username"]);
+
+            if (strlen($username) <2 ) {
+                // code...
+                $userErr = "Minimum 2 characters required";
+                $errCount = $errCount + 1;
+            }elseif (!preg_match("/^[a-zA-Z_\-.]*$/", $username)) {
+                $userErr = "Username can contain alpha numeric characters, period, dash or underscore only!";
+                $username ="";
+                $errCount = $errCount + 1;
+            } else{
+                if (isset($_POST['rmbm'])) {
+                    $time = time();
+                    setcookie('username', $username, $time + 120);
+                    setcookie('password', $password, $time + 120);
+                }
+            }
+
+        }
+
+        if (empty($_POST["password"])) {
+            $passErr = "Password is required";
+            $errCount = $errCount + 1;
+        } else {
+            $password = check_input($_POST["password"]);
+        }
+
+        if (strlen($password) <8 ) {
+            // code...
+            $passErr = "Minimum 8 characters required";
+            $errCount = $errCount + 1;
+        }
+
+        if (!preg_match("/^(?=.*[a-z])(?=.*[A-Z])(?!.* )(?=.*[%$#@]).+$/", $password)) {
+            /*
+            ^ starts the string
+            (?=.*[a-z]) Atleast a lower case letter
+            (?=.*[A-Z]) Atleast an upper case letter
+            (?!.* ) no space
+            (?=.*\d%$#@) Atleast a digit and atleast one of the specified characters.
+            .{8,16} between 8 to 16 characters
+            */
+            $passErr .= " Password must contain atleast a digit, a lower case and an upper case letter, atleast one of the [%$#@] and no space.";
+            $password ="";
+            $errCount = $errCount + 1;
+        }
+
+        if ($errCount < 1){
+            $time = time();
+            if (isset($_POST['rmbm'])) {
+                setcookie('username', $username, $time + 100);
+                setcookie('password', $password, $time + 100);
+            }
+
+            $strJsonFileContents = file_get_contents("data.json");
+            // var_dump($strJsonFileContents);
+
+            $arra = json_decode($strJsonFileContents);
+            // var_dump($arra);
+            $user_found = false;
+            foreach($arra as $item) { //foreach element in $arr
+                if ($username === $item->username){
+                    $user_found = true;
+                    // match. now check pw
+                    if ($password === $item->password){
+                        echo "Thanks for logging $item->name ... success!!";
+                        $_SESSION['uname'] = $username;
+                        $_SESSION['ugroup'] = $item->acc_group;
+                        header('Location: dashboard.php');
+                        //exit;
+                    }else{
+                        $passErr .= "Password Wrong!";
+                    }
+                }
+            }
+            if (!$user_found){
+                echo $userErr .= "No account found!";
+            }
+
+
+            //exit;
+
+        }
+
+    }
+}
+
+function check_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
+?>
+<br><br>
+<h4 style="color: white; text-align: center">You are not logged in! Please Login!</h4>
+
+<script>
+    function validateForm() {
+        let usrnm = document.forms["login-form"]["username"].value;
+        let passwrd = document.forms["login-form"]["password"].value;
+        if (usrnm === "") {
+            alert("Username must be filled out");
+            document.getElementById("text").innerHTML = "Username must be filled out";
+            document.getElementById("text").style.color="red";
+            return false;
+        } else if (passwrd === ""){
+            alert("Password must be filled out");
+            document.getElementById("text").innerHTML = "Password must be filled out";
+            document.getElementById("text").style.color="red";
+            return false;
+        }else{
+            document.getElementById("text").innerHTML = x;
+            document.getElementById("text").style.color="black";
+        }
+    }
+
+    function checkTextInput(){
+        let usrnm = document.forms["login-form"]["username"].value;
+        let passwrd = document.forms["login-form"]["password"].value;
+        if (usrnm !== "" && passwrd !== "") {
+            document.getElementById("sub_btn").hidden = false;
+            // document.getElementById("sub_btn").style.backgroundColor="green";
+        }
+
+    }
+
+</script>
+
+<div class="login-page">
+  <div class="form">
+    <form method="POST" class="login-form" name="login-form" onsubmit="return validateForm()">
+        <div style="text-align: left"> </div>
+        Username:
+        <span class="error">* <?php echo $userErr;?></span>
+        <input type="text" onkeyup="checkTextInput()" name="username" value="<?php if(isset($_COOKIE["username"])) { echo $_COOKIE["username"]; } ?>">
+
+        Password:
+        <span class="error">* <?php echo $passErr;?></span>
+        <input type="password" onkeyup="checkTextInput()" name="password" value="<?php if(isset($_COOKIE["password"])) { echo $_COOKIE["password"]; } ?>">
+        <br>
+
+        <input type="checkbox" id="rmbm" name="rmbm" value="True">
+        <label for="rmbm"> Remember Me</label><br><br>
+        <span id="text"></span>
+
+        <!-- style="background-color: gray; " --->
+        <button type="submit" id="sub_btn" value="Log in" hidden>login</button> <br> <br>
+
+        <a href="./forgot.php"> <span>Forgot Password?</span> </a> <br>
+        <a href="./registration.php"> <span>New Registration</span> </a>
+
+    </form>
+  </div>
+
+</div>
+
+
---
+++ Lab8/registration.php
@@ -0,0 +1,347 @@
+<?php
+include 'templates/nav2.php';
+include 'templates/base2.php';
+// define variables and set to empty values
+$nameErr = $emailErr = $degreeErr = $genderErr = $userErr = $passErr = $confrmPassErr = $dobErr = "";
+$name = $email = $gender = $username = $password = $cnfrmPass = "";
+$dob = $successmsg = "";
+$dobdd = $dobmm = $dobyy = "";
+$errCount = 0;
+$message = '';
+$error = '';
+if(isset($_POST["submit"]))  {
+
+    if (empty($_POST["name"])) {
+        $nameErr = "Name is required";
+        $errCount = $errCount + 1;
+    } else {
+        $name = check_input($_POST["name"]);
+        $wcount = str_word_count($name);
+        if ($wcount < 2 ) {
+            // code...
+            $nameErr = "Minimum 2 words required";
+            $errCount = $errCount + 1;
+        }
+
+        // check if name only contains letters and whitespace
+        if (!preg_match("/^[a-zA-Z]/", $name)) {
+            $nameErr = "Name must start with a letter!";
+            $name ="";
+            $errCount = $errCount + 1;
+        }
+
+        if (!preg_match("/^[a-zA-Z_\-. ]*$/",$name)) {
+            $nameErr = "Only letters, period and white space allowed";
+            $name="";
+            $errCount = $errCount + 1;
+        }
+    }
+
+    if (empty($_POST["email"])) {
+        $emailErr = "Email is required";
+        $errCount = $errCount + 1;
+    } else {
+        $email = check_input($_POST["email"]);
+        // check if e-mail address is well-formed
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $emailErr = "Invalid email format";
+            $email="";
+            $errCount = $errCount + 1;
+        }
+    }
+
+
+    if (empty($_POST["username"])) {
+        $userErr = "Username is required";
+        $errCount = $errCount + 1;
+    } else {
+        $username = $_POST["username"];
+
+        if (strlen($username) <2 ) {
+            // code...
+            $userErr = "Minimum 2 characters required";
+            $errCount = $errCount + 1;
+        }
+
+        // check if name only contains letters and whitespace
+        if (!preg_match("/^[a-zA-Z_\-.]*$/", $username)) {
+            $userErr = "Username can contain alpha numeric characters, period, dash or underscore only!";
+            $username ="";
+            $errCount = $errCount + 1;
+        }
+
+    }
+
+
+    if (empty($_POST["password"])) {
+        $passErr = "Password is required";
+        $errCount = $errCount + 1;
+    } else {
+
+        $password = check_input($_POST["password"]);
+        $cnfrmPass = check_input($_POST["cnfrmPass"]);
+
+        if (empty($cnfrmPass)) {
+            // code...
+            $confrmPassErr = "Confirm password is required";
+            $errCount = $errCount + 1;
+        } else {
+            if ($password != $cnfrmPass) {
+                // code...
+                $confrmPassErr = "Confirm password is didn't match with password!";
+                $errCount = $errCount + 1;
+            }
+        }
+
+
+        if (strlen($password) < 8 ) {
+            // code...
+            $passErr = "Minimum 8 characters required";
+            $errCount = $errCount + 1;
+        }
+
+        if (!preg_match("/^(?=.*[a-z])(?=.*[A-Z])(?!.* )(?=.*[%$#@]).+$/", $password)) {
+            /*
+            ^ starts the string
+                 (?=.*[a-z]) Atleast a lower case letter
+                 (?=.*[A-Z]) Atleast an upper case letter
+                 (?!.* ) no space
+                 (?=.*\d%$#@) Atleast a digit and atleast one of the specified characters.
+                 .{8,16} between 8 to 16 characters
+            */
+            $passErr .= " Password must contain atleast a digit, a lower case and an upper case letter, atleast one of the [%$#@] and no space.";
+            $password ="";
+            $errCount = $errCount + 1;
+        }
+
+    }
+
+    if (empty($_POST["gender"])) {
+        $genderErr = "Gender is required";
+        $errCount = $errCount + 1;
+    } else {
+        $gender = check_input($_POST["gender"]);
+    }
+
+    if (empty($_POST["dob"])) {
+        $dobErr = "Date of Birth is required";
+        $errCount = $errCount + 1;
+    } else {
+        $dob = $_POST["dob"];
+    }
+
+
+    if($errCount > 0) {
+        echo "<span class='error'>One or more error occurred!</span>";
+    } else {
+        if(file_exists('data.json'))
+        {
+            $current_data = file_get_contents('data.json');
+            $array_data = json_decode($current_data, true);
+            $extra = array(
+                'name'               =>     $_POST['name'],
+                'email'          =>     $_POST["email"],
+                'username'     =>     $_POST["username"],
+                'password'     =>     $_POST["password"],
+                'gender'     =>     $_POST["gender"],
+                'dob'     =>     $_POST["dob"],
+                'ppic_abs_path'     =>     '',
+                'status'     =>     'Active',
+                'acc_group'     =>     'Subscriber'
+            );
+            $array_data[] = $extra;
+            $final_data = json_encode($array_data);
+            if(file_put_contents('data.json', $final_data))
+            {
+                $message = "<label class='text-success'>Registration Success!</p>";
+                $name='';
+                $email='';
+                $username = $gender = $dob = '';
+
+            }
+        }
+        else
+        {
+            $error = 'JSON File not exits';
+        }
+    }
+}
+function check_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Registration</title>
+    <style>
+
+        .make-it-center{
+            margin: auto;
+            width: 50%;
+        }
+
+        body{
+            color: white;
+
+        }
+
+        .lefterr{
+            margin-left: -10%;
+        }
+
+        .required:after {
+            content:"*";
+            color: red;
+        }
+        .error{
+            color: red;
+        }
+
+        /* The sidebar menu */
+        .sidenav {
+            height: 100%; /* Full-height: remove this if you want "auto" height */
+            width: 220px; /* Set the width of the sidebar */
+            position: fixed; /* Fixed Sidebar (stay in place on scroll) */
+            z-index: 1; /* Stay on top */
+            top: 0; /* Stay at the top */
+            left: 0;
+            background-color: #111; /* Black */
+            overflow-x: hidden; /* Disable horizontal scroll */
+            padding-top: 20px;
+        }
+
+        /* The navigation menu links */
+        .sidenav a {
+            padding: 6px 8px 6px 16px;
+            text-decoration: none;
+            font-size: 25px;
+            color: #818181;
+            display: block;
+        }
+
+        /* When you mouse over the navigation links, change their color */
+        .sidenav a:hover {
+            color: #f1f1f1;
+        }
+
+    </style>
+    <script>
+        function validateForm() {
+            let usrnm = document.forms["reg_form"]["username"].value;
+            let passwrd = document.forms["reg_form"]["password"].value;
+            let name = document.forms["reg_form"]["name"].value;
+            let email = document.forms["reg_form"]["email"].value;
+            let cnfrmPass = document.forms["reg_form"]["cnfrmPass"].value;
+            let gender = document.forms["reg_form"]["gender"].value;
+            let dob = document.forms["reg_form"]["dob"].value;
+            let resp;
+            if (name === "") {
+                alert("Name must be filled out");
+                document.getElementById("res_text").innerHTML = "Name must be filled out";
+                document.getElementById("res_text").style.color="red";
+                resp = false;
+            }else if (email === "") {
+                alert("Email must be filled out");
+                document.getElementById("res_text").innerHTML = "Email must be filled out";
+                document.getElementById("res_text").style.color="red";
+                resp = false;
+            }else if (gender === "") {
+                alert("Gender must be filled out");
+                document.getElementById("res_text").innerHTML = "Gender must be filled out";
+                document.getElementById("res_text").style.color="red";
+                return false;
+            }else if (dob === "") {
+                alert("Date of Birth must be filled out");
+                document.getElementById("res_text").innerHTML = "Date of Birth must be filled out";
+                document.getElementById("res_text").style.color="red";
+                return false;
+            }else if (usrnm === "") {
+                alert("Username must be filled out");
+                document.getElementById("res_text").innerHTML = "Username must be filled out";
+                document.getElementById("res_text").style.color="red";
+                return false;
+            } else if (passwrd === ""){
+                alert("Password must be filled out");
+                document.getElementById("res_text").innerHTML = "Password must be filled out";
+                document.getElementById("res_text").style.color="red";
+                return false;
+            }else if (cnfrmPass === "") {
+                alert("Confirm password must be filled out");
+                document.getElementById("res_text").innerHTML = "Confirm password must be filled out";
+                document.getElementById("res_text").style.color="red";
+                return false;
+            }else{
+                document.getElementById("res_text").innerHTML = x;
+                document.getElementById("res_text").style.color="black";
+            }
+        }
+
+        function checkTextInput(){
+            let usrnm = document.forms["reg_form"]["username"].value;
+            let passwrd = document.forms["reg_form"]["password"].value;
+            if (usrnm !== "" && passwrd !== "") {
+                document.getElementById("sub_btn").hidden = false;
+                // document.getElementById("sub_btn").style.backgroundColor="green";
+            }
+
+        }
+
+    </script>
+</head>
+
+<body>
+<div class="donor-info make-it-center">
+<br />
+<div class="container" style="width:500px;">
+    <h3>User Registration</h3>
+    <form name="reg_form" onsubmit="return validateForm()" method="post">
+        <?php
+        if(isset($error))
+        {
+            echo $error;
+        }
+        ?>
+        <br />
+        <label>Name</label>  <span class="error">* <?php echo $nameErr;?></span>
+        <input type="text" name="name" class="form-control" value="<?php echo $name;?>" /> <br/><br/>
+        <label>E-mail</label> <span class="error">* <?php echo $emailErr;?></span>
+        <input type="text" name = "email" class="form-control" value="<?php echo $email;?>" /><br /><br/>
+        <label>User Name</label>  <span class="error">* <?php echo $userErr;?></span>
+        <input type="text" name = "username" class="form-control" value="<?php echo $username;?>" /><br /><br/>
+        <label>Password</label>  <span class="error">* <?php echo $passErr;?></span>
+        <input type="password" name = "password" class="form-control" /><br /><br/>
+        <label>Confirm Password</label>  <span class="error">* <?php echo $confrmPassErr;?></span>
+        <input type="password" name = "cnfrmPass" class="form-control" /><br /><br/>
+
+        <fieldset>
+            <legend>Gender</legend>  <span class="error">* <?php echo $genderErr;?></span>
+            <input type="radio" id="male" name="gender" value="male" <?php if ($gender === 'male'){ echo 'checked';}?> >
+            <label for="male">Male</label>
+            <input type="radio" id="female" name="gender" value="female" <?php if ($gender === 'female'){ echo 'checked';}?> >
+            <label for="female">Female</label>
+            <input type="radio" id="other" name="gender" value="other" <?php if ($gender === 'other'){ echo 'checked';}?> >
+            <label for="other">Other</label><br>
+
+            <legend>Date of Birth:</legend>  <span class="error">* <?php echo $dobErr;?></span>
+            <input type="date" name="dob" value="<?php echo $dob;?>"> <br><br>
+        </fieldset> <br>
+
+        <span id="res_text"></span> <br>
+
+        <input disabled type="submit" id="sub_btn" name="submit" value="Register" class="btn btn-info" /><br />
+        <?php
+        if(isset($message))
+        {
+            echo $message;
+        }
+        ?>
+    </form>
+</div>
+<br />
+</div>
+</body>
+</html>
