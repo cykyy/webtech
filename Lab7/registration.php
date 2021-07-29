5c2f35b465c08a2c00f0f33c56c85ce1260367e6
Rayhan Mia <miarayhan11@icloud.com>
2021-07-29 20:06:37 +0600

    lab

--- Lab7/registration.php
+++ Lab7/registration.php
@@ -172,58 +172,7 @@ function check_input($data) {
 <html>
 <head>
     <title>Registration</title>
-    <style>
-
-        .make-it-center{
-            margin: auto;
-            width: 50%;
-        }
-
-        body{
-            color: white;
-
-        }
-
-        .lefterr{
-            margin-left: -10%;
-        }
-
-        .required:after {
-            content:"*";
-            color: red;
-        }
-        .error{
-            color: red;
-        }
-
-        /* The sidebar menu */
-        .sidenav {
-            height: 100%; /* Full-height: remove this if you want "auto" height */
-            width: 220px; /* Set the width of the sidebar */
-            position: fixed; /* Fixed Sidebar (stay in place on scroll) */
-            z-index: 1; /* Stay on top */
-            top: 0; /* Stay at the top */
-            left: 0;
-            background-color: #111; /* Black */
-            overflow-x: hidden; /* Disable horizontal scroll */
-            padding-top: 20px;
-        }
-
-        /* The navigation menu links */
-        .sidenav a {
-            padding: 6px 8px 6px 16px;
-            text-decoration: none;
-            font-size: 25px;
-            color: #818181;
-            display: block;
-        }
-
-        /* When you mouse over the navigation links, change their color */
-        .sidenav a:hover {
-            color: #f1f1f1;
-        }
-
-    </style>
+    <link rel="stylesheet" href="staticfiles/extras.css">
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <script>
         function validateForm() {
--- Lab7/templates/base2.php
+++ Lab7/templates/base2.php
@@ -3,7 +3,6 @@
 <head>
     <meta charset="UTF-8">
     <title>Cartista</title>
-    <link rel="stylesheet" type="text/css" href="staticfiles/clients_style.css">
     <link rel="stylesheet" type="text/css" href="staticfiles/footer_base.css">
     <link rel="stylesheet" type="text/css" href="staticfiles/admin_login.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
